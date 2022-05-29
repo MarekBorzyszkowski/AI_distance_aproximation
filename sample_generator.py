cd21d2068697fdfb8d1967a25e0a53b685d314f7
Marek Borzyszkowski <marek.borzyszkowski2@gmail.com>
2022-05-30 00:45:45 +0200

    3DModel

--- sample_generator.py
+++ sample_generator.py
@@ -7,7 +7,8 @@ import numpy as np
 
 
 def save_to_csv(result, file_name, folder_name, numbers_per_point):
-    header = ['a{}'.format(i) for i in range(1, numbers_per_point + 1)] +['distance']# ['b{}'.format(i) for i in range(1, numbers_per_point + 1)]
+    header = ['a{}'.format(i) for i in range(1, numbers_per_point + 1)] + \
+             ['b{}'.format(i) for i in range(1, numbers_per_point + 1)] + ['distance']
 
     with open('csv/' + folder_name + '/' + file_name, 'w') as f:
         writer = csv.writer(f)
@@ -35,7 +36,7 @@ def create_distance_list(dimension, num_of_samples, samples1, samples2):
     distance_x1_x2 = []
     for i in range(num_of_samples):
         distance_x1_x2.append(distance_measure(samples1[i], samples2[i]) * (dimension ** 0.5) / dimension)
-        if(distance_x1_x2[i] >=1.0):
+        if distance_x1_x2[i] >= 1.0:
             distance_x1_x2[i] = 1.0
     return distance_x1_x2
 
@@ -43,7 +44,7 @@ def create_distance_list(dimension, num_of_samples, samples1, samples2):
 def distance_measure(list1, list2):
     sum = 0
     for i in range(len(list1)):
-        sum += (list1[i]-list2[i])**2
+        sum += (list1[i] - list2[i]) ** 2
     return sum ** 0.5
 
 
@@ -77,7 +78,7 @@ def average(sample):
 
 
 def euclidean_norm(sample):
-    return np.linalg.norm(sample)/len(sample)
+    return np.linalg.norm(sample) / len(sample)
 
 
 def inf_norm(sample):
@@ -88,7 +89,7 @@ def abs_a_minus_b_samples():
     result = []
     for a in range(100):
         for b in range(100):
-            result.append([a/100.0, b/100.0, abs(a-b)/100.0])
+            result.append([a / 100.0, b / 100.0, abs(a - b) / 100.0])
     validate_data(result)
     return result
 
@@ -96,13 +97,13 @@ def abs_a_minus_b_samples():
 def abs_a_minus_b_samples_random(num_of_samples):
     result = [[random.uniform(0, 1.0) for j in range(2)] for i in range(num_of_samples)]
     for row in result:
-        row.append(abs(row[0]-row[1]))
+        row.append(abs(row[0] - row[1]))
     validate_data(result)
     return result
 
 
 def x_squared_samples():
-    result = [[i/100, (i/100)**2] for i in range(100)]
+    result = [[i / 100, (i / 100) ** 2] for i in range(100)]
     validate_data(result)
     return result
 
@@ -110,12 +111,25 @@ def x_squared_samples():
 def x_squared_samples_random(num_of_samples):
     result = [[random.uniform(0, 1.0)] for i in range(num_of_samples)]
     for row in result:
-        row.append(row[0]**2)
+        row.append(row[0] ** 2)
     validate_data(result)
     return result
 
 
-
+def generate_discrete_samples_3d():
+    points1 = []
+    points2 = []
+    for a in range(10):
+        for b in range(10):
+            for c in range(10):
+                for d in range(10):
+                    for e in range(10):
+                        for f in range(10):
+                            points1.append([a / 10.0, b / 10.0, c/10.0])
+                            points2.append([d/10.0, e/10.0, f/10.0])
+    distance_value = create_distance_list(3, 10 ** 6, points1, points2)
+    result = [points1[i] + points2[i] + [distance_value[i]] for i in range(10 ** 6)]
+    return result
 
 
 def validate_data(result):
@@ -124,10 +138,9 @@ def validate_data(result):
             if num >= 1.0:
                 num = 1.0
 
-
 # generate_sample_file(3, 1000, 'test')
 
-#generate_sample_file_with_zeros(3, 1, 1000, 'test')
+# generate_sample_file_with_zeros(3, 1, 1000, 'test')
 
 # result = []
 # for i in range(1, 21):
@@ -136,9 +149,7 @@ def validate_data(result):
 # result = np.array(result)
 # result = shuffle(result)
 # save_to_csv(result, 'dmn2000000norm.csv', 'training', 3)
-# for i in range(20):
-#     save_to_csv(generate_samples(32, 1000000), 'd32n1000000_s{}.csv'.format(i), 'training/32DimBatch', 32)
+# for i in range(5):
+#     save_to_csv(generate_samples(3, 1000000), 'd3n1000000_s{}.csv'.format(i), 'training/3DimBatch', 3)
 
-save_to_csv(x_squared_samples_random(100000),'square_100000.csv', 'test/square', 1)
-
-
+save_to_csv(generate_discrete_samples_3d(),'d3_1000000.csv', 'training/3DimDiscrete', 3)
--- trainer.py
+++ trainer.py
@@ -1,6 +1,7 @@
 from keras.metrics import MeanSquaredError
 from sklearn.utils import shuffle
 from tensorflow import keras
+import tensorflow as tf
 print('Tensorflow/Keras: %s' % keras.__version__)
 
 from keras.models import Sequential
@@ -27,59 +28,67 @@ import os
 def print_data(expected, predicted):
     print([[expected[i], predicted[i], abs(expected[i] - predicted[i])] for i in range(len(expected))])
 
-
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 # main_dir = os.path.dirname(sys.path[0])
 
-dimension = 32
+dimension = 3
 samples = 1000000
 labels = ['a{}'.format(i) for i in range(1, dimension + 1)] + \
          ['b{}'.format(i) for i in range(1, dimension + 1)]
-#
-# model2 = Sequential()
-# model2.add(Input(shape=(2 * dimension,)))
-# model2.add(Dense(units=4*dimension, activation='relu', input_dim=2*dimension))
-# model2.add(Dense(units=6*dimension, activation='relu'))
-# model2.add(Dense(units=1, activation='relu'))
-# model2.compile(optimizer='adam', loss='mse', metrics=[MeanSquaredError(), 'Precision'])
 
-model2 = keras.models.load_model('models/32dim/feedforward_distance_in_32D_v4.h5')
+model2 = Sequential()
+model2.add(Input(shape=(2 * dimension,)))
+model2.add(Dense(units=8*dimension, activation='relu', input_dim=2*dimension))
+model2.add(Dense(units=8*dimension, activation='relu'))
+model2.add(Dense(units=1, activation='relu'))
+model2.compile(optimizer='adam', loss='mse', metrics=[MeanSquaredError(), 'Precision'])
 
-for i in range(5,20):
-    df = pd.read_csv('csv/training/32DimBatch/d32n1000000_s{}.csv'.format(i))
-    X = df[labels]
-    y = df['distance'].values
-
-    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.0, random_state=0)
-
-    X_train = np.array(X)
-    #X_test = np.array(X_test)
-    y_train = np.array(y)
-    #y_test = np.array(y_test)
-
-    X_train, y_train = shuffle(X_train, y_train)
-
-    model2.fit(x=X_train,  # input data
-               y=y_train,  # target data
-               batch_size=32,  # Number of samples per gradient update.
-               epochs=100, #Number of epochs to train the model.
-               verbose=1,
-               validation_split=0.2,
-               shuffle=True,
-               validation_freq=3,
-               )
+# model2 = keras.models.load_model('models/32dim/feedforward_distance_in_32D_v4.h5')
+df = pd.read_csv('csv/training/3DimDiscrete/d3_1000000.csv')
+X = df[labels]
+y = df['distance'].values
+df2 = pd.read_csv('csv/training/3DimBatch/d3n1000000_s0.csv')
+X2 = df[labels]
+y2 = df['distance'].values
 
 
-    print("")
-    print('-------------------- Model Summary --------------------')
-    model2.summary()  # print model summary
-    print("")
-    print('-------------------- Weights and Biases --------------------')
-    for layer in model2.layers:
-        print("Layer: ", layer.name)  # print layer name
-        print("  --Kernels (Weights): ", layer.get_weights()[0])  # kernels (weights)
-        print("  --Biases: ", layer.get_weights()[1])  # biases
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.0, random_state=0)
 
-    model2.save('models/32dim/feedforward_distance_in_32D_v{}.h5'.format(i))
+X_train = np.array(X)
+#X_test = np.array(X_test)
+y_train = np.array(y)
+#y_test = np.array(y_test)
+X_valid = np.array(X2)
+y_valid = np.array(y2)
+
+X_train, y_train = shuffle(X_train, y_train)
+X_valid, y_valid = shuffle(X_valid, y_valid)
+ckp_cbk = tf.keras.callbacks.ModelCheckpoint('models/3DimDiscrete/1_model', save_best_only=True, monitor='loss')
+tb_cbk = tf.keras.callbacks.TensorBoard(log_dir='models/3DimDiscrete/1_model/logs')
+
+model2.fit(x=X_train,  # input data
+           y=y_train,  # target data
+           batch_size=32,  # Number of samples per gradient update.
+           epochs=100000, #Number of epochs to train the model.
+           verbose=1,
+           validation_data=(X_valid, y_valid),
+           shuffle=True,
+           validation_freq=3,
+           callbacks=[ckp_cbk, tb_cbk]
+           )
+
+
+print("")
+print('-------------------- Model Summary --------------------')
+model2.summary()  # print model summary
+print("")
+print('-------------------- Weights and Biases --------------------')
+for layer in model2.layers:
+    print("Layer: ", layer.name)  # print layer name
+    print("  --Kernels (Weights): ", layer.get_weights()[0])  # kernels (weights)
+    print("  --Biases: ", layer.get_weights()[1])  # biases
+
+model2.save('models/3DimDiscrete/feedforward_distance_in_3D_discrete.h5')
 
 # pred_labels_tr = model2.predict(X_train)
 # pred_labels_te = model2.predict(X_test)
